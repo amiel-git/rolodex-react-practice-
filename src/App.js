82f948c2b1ad0e21eab55f7896306576565a58a2
Amiel <afurongamielchrysdin@gmail.com>
2021-01-13 01:02:21 +0800

    Style change

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ class App extends Component {
     ));
 
     return (
+      
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox placeholder="Search for monsters" 
         handleChange={this.handleChange}/>
       
